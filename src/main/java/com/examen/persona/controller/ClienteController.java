3c8d4cfe665e66dbb5b33fa40730e362f32dc6f6
Daniotrix <teamdglsoft@gail.com>
2020-04-30 21:18:55 -0500

    Se configuro la app para aceptar conexiones cruzadas cors

--- src/main/java/com/examen/persona/controller/ClienteController.java
+++ src/main/java/com/examen/persona/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.examen.persona.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +11,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.examen.persona.dto.Persona;
 import com.examen.persona.service.PersonaServiceImpl;
 
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,
+		RequestMethod.POST,
+		RequestMethod.DELETE,
+		RequestMethod.PUT})
 @RestController
 @RequestMapping("/api")
 public class ClienteController {
